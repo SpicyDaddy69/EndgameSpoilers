95d5697ee5a466aaaecf97fac4747e036fcaaa98
SpicyDaddy69 <50152058+SpicyDaddy69@users.noreply.github.com>
2019-05-03 13:14:30 -0500

    Delete TreasureHunter.java

--- TreasureHunter.java
+++
@@ -1,8 +0,0 @@
-
-public class TreasureHunter extends Human
-{
-	public TreasureHunter(int str, int end, int agil, int intel, int per, String name, String gender)
-	{
-		super(str, end, agil, intel, per, name, gender);
-	}
-}
