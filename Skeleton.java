e41913589254dc23b835fdaf6098e3288ec9fc35
SpicyDaddy69 <50152058+SpicyDaddy69@users.noreply.github.com>
2019-05-03 13:14:20 -0500

    Delete Skeleton.java

--- Skeleton.java
+++
@@ -1,8 +0,0 @@
-
-public class Skeleton extends Character
-{
-	public Skeleton(int str, int end, int agil, int intel, int per, String name, String gender)
-	{
-		super(str, end, agil, intel, per, name, gender);
-	}
-}
