cf6971d2fd968b49a0e20593fb44687c2fbf1c0b
SpicyDaddy69 <50152058+SpicyDaddy69@users.noreply.github.com>
2019-05-03 13:13:02 -0500

    Delete Bandit.java

--- Bandit.java
+++
@@ -1,8 +0,0 @@
-
-public class Bandit extends Human
-{
-	public Bandit(int str, int end, int agil, int intel, int per, String name, String gender)
-	{
-		super(str, end, agil, intel, per, name, gender);
-	}
-}
