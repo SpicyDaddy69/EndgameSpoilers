2e6f222180c594c33185bd8c3a9b8083f3ee3a62
SpicyDaddy69 <50152058+SpicyDaddy69@users.noreply.github.com>
2019-05-03 13:13:52 -0500

    Delete Goblin.java

--- Goblin.java
+++
@@ -1,8 +0,0 @@
-
-public class Goblin extends Character
-{
-	public Goblin(int str, int end, int agil, int intel, int per, String name, String gender)
-	{
-		super(str, end, agil, intel, per, name, gender);
-	}
-}
