83b77352ccd805a1e8ec024692d41135607ea16c
SpicyDaddy69 <50152058+SpicyDaddy69@users.noreply.github.com>
2019-05-03 13:13:45 -0500

    Delete Ghost.java

--- Ghost.java
+++
@@ -1,8 +0,0 @@
-
-public class Ghost extends Character
-{
-	public Ghost(int str, int end, int agil, int intel, int per, String name, String gender)
-	{
-		super(str, end, agil, intel, per, name, gender);
-	}
-}
