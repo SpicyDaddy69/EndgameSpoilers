219fcab2b1c89edd2710e997cf3739f42cb2af09
SpicyStalin <50152110+SpicyStalin@users.noreply.github.com>
2019-05-09 12:47:14 -0500

    Add files via upload
    -Changed BattleMenu from a JFrame to a JDialog to allow for easier swapping from movement to battle
    -Dimensions of BattleMenu changed from 1920 x 1080 to 1800 x 1000 to make it easier to view due to the lack of a fullscreen option

--- GUI/src/BattleMenu.java
+++ GUI/src/BattleMenu.java
@@ -2,17 +2,23 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class BattleMenu extends JFrame implements ActionListener
+public class BattleMenu extends JDialog implements ActionListener
 {
 	private JButton attack;
 	private JButton escape;
 	private JPanel menu;
 	private JPanel display1;
 	private JPanel display2;
+	private JPanel display3;
 	private Container contentPane;
 	private JLabel image1;
 	private JLabel image2;
+	private JLabel image3;
 	private Characters player, enemy;
+	private JLabel health1;
+	private JLabel health2;
+	private JTextArea events;
+	private JScrollPane scroll;
 	
 	public BattleMenu(int width, int height, Characters char1, Characters char2)
 	{	
@@ -27,14 +33,33 @@ public class BattleMenu extends JFrame implements ActionListener
 		escape = new JButton("Escape");
 		
 		image1 = new JLabel(new ImageIcon("placeholder.png"));
-		display1 = new JPanel();
-		display1.add(image1);
+		health1 = new JLabel("Health: " + player.getHealth());
+		health1.setForeground(Color.GREEN);
+		display1 = new JPanel(new BorderLayout());
+		display1.add(image1, BorderLayout.NORTH);
+		display1.add(health1, BorderLayout.CENTER);
 		display1.setBackground(Color.DARK_GRAY);
 		
 		image2 = new JLabel(new ImageIcon("placeholder.png"));
-		display2 = new JPanel();
-		display2.add(image2);
+		health2 = new JLabel("Health: " + enemy.getHealth());
+		health2.setForeground(Color.GREEN);
+		display2 = new JPanel(new BorderLayout());
+		display2.add(image2, BorderLayout.NORTH);
+		display2.add(health2, BorderLayout.CENTER);
 		display2.setBackground(Color.DARK_GRAY);
+		
+		events = new JTextArea(5, 20);
+		events.setEditable(false);
+		events.setFont(new Font("Serif", Font.PLAIN, 20));
+		
+		scroll = new JScrollPane(events, scroll.VERTICAL_SCROLLBAR_AS_NEEDED, scroll.HORIZONTAL_SCROLLBAR_NEVER);
+		scroll.setPreferredSize(new Dimension(400, 110));
+		
+		image3 = new JLabel(new ImageIcon("placeholder.png"));
+		display3 = new JPanel(new BorderLayout());
+		display3.add(image3, BorderLayout.NORTH);
+		display3.add(scroll, BorderLayout.SOUTH);
+		display3.setBackground(Color.DARK_GRAY);
 		
 		menu = new JPanel();
 		menu.add(attack);
@@ -45,6 +70,7 @@ public class BattleMenu extends JFrame implements ActionListener
 		contentPane.add(menu, BorderLayout.SOUTH);
 		contentPane.add(display1, BorderLayout.WEST);
 		contentPane.add(display2, BorderLayout.EAST);
+		contentPane.add(display3, BorderLayout.CENTER);
 	}
 		
 	@Override
@@ -52,8 +78,12 @@ public class BattleMenu extends JFrame implements ActionListener
 	{
 		if(e.getSource() == attack)
 		{
-			BattleMechanics test = new BattleMechanics(player);
-			System.out.println(test.attack(1, enemy));
+			BattleMechanics playerAttack = new BattleMechanics(player);
+			BattleMechanics enemyAttack = new BattleMechanics(enemy);
+			events.append(playerAttack.attack(1, enemy));
+			health1.setText("Health: " + player.getHealth());
+			events.append(enemyAttack.attack(1, player));
+			health2.setText("Health: " + enemy.getHealth());
 		}
 		else
 			System.out.println("You ran");
